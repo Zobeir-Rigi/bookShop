58d5f8539ed505bb0be49a3d42af3c52a011785d
zobeir-rigi <rigi.zobir@yahoo.com>
2025-05-22 06:50:21 +0100

    main.terraform created

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+// I choose AWS as a provider so I need this : 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1" // it should be my profile region
+}
